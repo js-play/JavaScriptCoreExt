7ac513a8b8009fffff5ffb6dd0c59b67bc2eb3d8
Dj <43033058+DjDeveloperr@users.noreply.github.com>
2023-01-21 23:21:47 +0530

    huh

--- Sources/JavaScriptCoreExt/JavaScriptCoreExt.swift
+++ Sources/JavaScriptCoreExt/JavaScriptCoreExt.swift
@@ -8,35 +8,36 @@ public enum JSScriptType: Int32 {
     case program, module
 }
 
-// public class JSCExtScript {
-//     var inner: Any
+public class JSCExtScript {
+    var inner: Any
     
-//     init(
-//         ofType type: JSScriptType,
-//         withSource source: String,
-//         andSourceURL url: URL,
-//         in vm: JSVirtualMachine
-//     ) throws {
-//         let cls = objc_getClass("JSScript")!
-//         let sel = Selector(("scriptOfType:withSource:andSourceURL:andBytecodeCache:inVirtualMachine:error:"))
-//         typealias Fn = @convention(c) (Any, Selector, CInt, NSString, NSURL, Any?, Any, UnsafeMutablePointer<NSError?>) -> Any
-//         let fn = unsafeBitCast(msgSend, to: Fn.self)
-//         let outError = UnsafeMutablePointer<NSError?>.allocate(capacity: 1)
-//         inner = fn(
-//             cls,
-//             sel,
-//             type.rawValue,
-//             NSString(string: source),
-//             NSURL(string: url.absoluteString)!,
-//             nil,
-//             vm,
-//             outError
-//         )
-//         if let err = outError.pointee {
-//             throw err
-//         }
-//     }
-// }
+    init(
+        ofType type: JSScriptType,
+        withSource source: String,
+        andSourceURL url: URL,
+        in vm: JSVirtualMachine
+    ) throws {
+        print("huh")
+        let cls = objc_getClass("JSScript")!
+        let sel = Selector(("scriptOfType:withSource:andSourceURL:andBytecodeCache:inVirtualMachine:error:"))
+        typealias Fn = @convention(c) (Any, Selector, CInt, NSString, NSURL, Any?, Any, UnsafeMutablePointer<NSError?>) -> Any
+        let fn = unsafeBitCast(msgSend, to: Fn.self)
+        let outError = UnsafeMutablePointer<NSError?>.allocate(capacity: 1)
+        inner = fn(
+            cls,
+            sel,
+            type.rawValue,
+            NSString(string: source),
+            NSURL(string: url.absoluteString)!,
+            nil,
+            vm,
+            outError
+        )
+        if let err = outError.pointee {
+            throw err
+        }
+    }
+}
 
 @objc public protocol JSModuleLoaderDelegate {
     @objc func context(_ context: JSContext!, fetchModuleForIdentifier identifier: JSValue!, withResolveHandler resolve: JSValue!, andRejectHandler reject: JSValue!)
